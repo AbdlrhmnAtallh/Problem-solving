67293a4a8bd53bb2217fbe8ba3974e1a87d98b3f
AbdlrhmnAtallh <121711667+AbdlrhmnAtallh@users.noreply.github.com>
2023-04-25 01:59:00 +0200

    Add files via upload

---
+++ 008 - Rot13 Cipher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Rot13_Cipher
+{
+    class Rot13Cipher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your message: ");
+            string message = Console.ReadLine();
+
+            string encryptedMessage = Encrypt(message);
+            string decryptedMessage = Decrypt(encryptedMessage);
+
+            Console.WriteLine("Encrypted message: " + encryptedMessage);
+            Console.WriteLine("Decrypted message: " + decryptedMessage);
+
+            Console.ReadKey();
+        }
+
+        static string Encrypt(string message)
+        {
+            char[] charArray = message.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (char.IsLower(charArray[i]))
+                {
+                    charArray[i] = (char)(((charArray[i] - 'a') + 13) % 26 + 'a');
+                }
+                else if (char.IsUpper(charArray[i]))
+                {
+                    charArray[i] = (char)(((charArray[i] - 'A') + 13) % 26 + 'A');
+                }
+            }
+
+            return new string(charArray);
+        }
+
+        static string Decrypt(string encryptedMessage)
+        {
+            char[] charArray = encryptedMessage.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (char.IsLower(charArray[i]))
+                {
+                    charArray[i] = (char)(((charArray[i] - 'a') + 13) % 26 + 'a');
+                }
+                else if (char.IsUpper(charArray[i]))
+                {
+                    charArray[i] = (char)(((charArray[i] - 'A') + 13) % 26 + 'A');
+                }
+            }
+
+            return new string(charArray);
+        }
+    }
+}

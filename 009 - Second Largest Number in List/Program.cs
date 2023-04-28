31b8ab45982fbad69c0ccab523c294db3d68911b
AbdlrhmnAtallh <121711667+AbdlrhmnAtallh@users.noreply.github.com>
2023-04-28 22:20:45 +0200

    Add files via upload

---
+++ 009 - Second Largest Number in List/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Second_Largest_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter the size of list ");
+             int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter the list of numbers \n");
+            int[] numbers = new int[size];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < numbers.Length-1; i++) // Time Complexity: O(N^2)
+            {
+                for (int j = 0; j < numbers.Length- i - 1; j++)
+                {
+                     if (numbers[j] > numbers[j + 1])
+                     {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                     }
+                }
+            }
+            Console.WriteLine(" The Second Longest Numbers is: "+ numbers[size - 2]);
+
+        }
+    }
+}

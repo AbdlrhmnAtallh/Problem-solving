c26085c21a6b6f2b49ff956d15964b9a08ce5437
Abdelrhman Mohamed <121711667+AbdlrhmnAtallh@users.noreply.github.com>
2023-08-07 00:22:49 +0300

    Add files via upload

---
+++ 014 - Generic Stack/GenericStack/GenericStack/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GenericStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> stack = new Stack<int>();
+            stack.push(10);
+            stack.push(20);
+            stack.push(30);
+
+            while (!stack.IsEmpty())
+            {
+                int item = stack.pop();
+                Console.WriteLine(item);
+            }
+
+            Stack<char> stack1 = new Stack<char>();
+            stack1.push('n');
+            stack1.push('a');
+            stack1.push('m');
+            stack1.push('h');
+            stack1.push('r');
+            stack1.push('l');
+            stack1.push('e');
+            stack1.push('d');
+            stack1.push('b');
+            stack1.push('a');
+            
+            while (!stack1.IsEmpty())
+            {
+                char item = stack1.pop();
+                Console.Write(item);
+            }
+            
+            
+        }
+    }
+}
---
+++ 014 - Generic Stack/GenericStack/GenericStack/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStack
+{
+    internal class Stack<T>
+    {
+        private List<T> Items;
+        public Stack ()
+        {
+            Items = new List<T>();
+        }
+        
+        public void push (T item)
+        {
+            Items.Add(item);
+        }
+
+        public T pop ()
+        {
+            if (Items.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty ");
+            }
+            T item = Items[Items.Count - 1];
+            Items.RemoveAt(Items.Count - 1);
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return Items.Count == 0;
+        }
+
+    }
+}

496d576ad3e5eeae08fa5074067e87d473c9a69a
Abdelrhmn <abdlrhmntt@gmail.com>
2024-03-14 16:13:25 +0200

    Mastery achieved: Problem successfully tackled and code perfected.

---
+++ 019 - Staircase detail/StaircaseDetail/StaircaseDetail/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StaircaseDetail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Staircase(5);
+        }
+        public static void Staircase(int n)
+        {
+            int x = n - 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == x)
+                    {
+                        Console.Write("#");
+                        x--;
+                    }
+                    else if (j > x)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
